41da69c2a5d6aead6813797038a2aefe13952b68
Djan Tanova <guralcan@gmail.com>
2026-04-09 14:57:07 +0300

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", 
+  base: "/",
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
   worker: {
     format: "es",
   },
